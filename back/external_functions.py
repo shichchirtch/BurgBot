8fa8b5d5a192a09c5eda7c89002f8e9b05cfbed1
shichchirtch <ivanvol9l@gmail.com>
2025-12-30 23:14:14 +0100

    1 commit INIT

---
+++ back/external_functions.py
@@ -0,0 +1,27 @@
+from aiogram.types import CallbackQuery
+from aiogram_dialog.api.entities.modes import ShowMode
+from aiogram_dialog.widgets.kbd import ManagedRadio
+from aiogram_dialog import DialogManager
+from redis_client import r
+
+
+async def get_user_count():
+    users_started_bot_allready = await r.scard("users")  # Считаю юзеров
+    return users_started_bot_allready
+
+
+async def radio_spam_button_clicked(callback: CallbackQuery,
+                                    radio: ManagedRadio,
+                                    dialog_manager: DialogManager, *args, **kwargs):
+    temp_dict = {'1': 'Ну и ладно', '2': 'Очень хорошо'}
+
+    await callback.message.answer(f"{temp_dict[callback.data[-1]]}")
+    dialog_manager.show_mode = ShowMode.SEND
+    await dialog_manager.next()
+
+
+
+
+
+
+
